bcc66e62d9d81248f281e0755a7c2959ee3afcaf
Marwan <marwan.hossam@gmail.com>
2023-08-11 09:25:19 +0200

    update celery

--- backend/IoTDashboard/celery.py
+++ backend/IoTDashboard/celery.py
@@ -3,10 +3,11 @@ from __future__ import absolute_import
 from datetime import timedelta
 
 from celery import Celery, signals
+from celery.schedules import crontab
 
-from IoTDashboard.constants import CELERY_APP
-from IoTDashboard.services.sentry import init_sentry
-from IoTDashboard.settings import CELERY_BROKER_URL, CELERY_RESULT_BACKEND
+from .constants import CELERY_APP
+from .services.sentry import init_sentry
+from .settings import CELERY_BROKER_URL, CELERY_RESULT_BACKEND
 
 celery_app = Celery(
     __name__,
@@ -14,6 +15,9 @@ celery_app = Celery(
     broker=CELERY_BROKER_URL,
 )
 
+
+publish_temperature_data_task = "publish_temperature_data"
+ingest_temperature_data_task = "ingest_temperature_data"
 
 # Setting up Sentry for Celery
 @signals.celeryd_init.connect
@@ -25,3 +29,9 @@ celery_app.conf.result_expires = timedelta(hours=1)  # TTL for celery task resul
 celery_app.conf.task_create_missing_queues = True
 celery_app.conf.timezone = "UTC"
 celery_app.autodiscover_tasks(["IoTDashboard"])
+celery_app.conf.beat_schedule = {
+    "run-every-minute": {
+        "task": publish_temperature_data_task,
+        "schedule": crontab(),  # Default 60 seconds (1 minute)
+    },
+}
