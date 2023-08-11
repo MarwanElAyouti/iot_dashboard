3c316f2ed4751f30904d38d8e0f2af7f0a99369c
Marwan <marwan.hossam@gmail.com>
2023-08-11 09:24:31 +0200

    update router

--- backend/IoTDashboard/router.py
+++ backend/IoTDashboard/router.py
@@ -1,10 +1,13 @@
-from fastapi import APIRouter, Depends
+from datetime import datetime, timedelta
 
-from IoTDashboard.services.jwt import get_auth_user
+from fastapi import APIRouter, Query, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+
+from . import models, schemas
+from .db import get_db
+from .services.jwt import get_auth_user
 
 router = APIRouter()
-
-# Add routers here via router.include_router({router_name})
 
 
 @router.get("/health")
@@ -15,3 +18,33 @@ async def health():
 @router.get("/test_jwt")
 async def test_jwt(user: dict = Depends(get_auth_user)):
     return {"status": "Authentication Passed"}
+
+
+@router.get("/sensors", response_model=list[schemas.Sensor])
+async def get_sensors(db: Session = Depends(get_db)) -> list[schemas.Sensor]:
+    sensors = db.query(models.Sensor).all()
+    return [schemas.Sensor(created_at=datetime.now(), id=sensor.id, name=sensor.name) for sensor in sensors]
+
+
+@router.post("/sensors", status_code=status.HTTP_201_CREATED)
+async def add_sensor(sensor: schemas.Sensor, db: Session = Depends(get_db)):
+    try:
+        record = models.Sensor(name=sensor.name, created_at=datetime.now())
+        db.add(record)
+        db.commit()
+    except Exception:
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
+    return {"detail": f"Sensor (id={record.id}) has been sucessfully created."}
+
+
+@router.get("/events", response_model=list[schemas.Event])
+async def get_events(delta: int = Query(default=10, example=10), db: Session = Depends(get_db)) -> list[schemas.Event]:
+    lookup_time = datetime.now() - timedelta(minutes=delta)
+    events = db.query(models.Event).filter(models.Event.created_at >= lookup_time).all()
+
+    return [
+        schemas.Event(
+            id=event.id, temperature=event.temperature, created_at=event.created_at, sensor_id=event.sensor_id
+        )
+        for event in events
+    ]
